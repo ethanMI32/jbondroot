756d381f357e5fdb68f31c7072e89be36480feec
ethanMI32 <140582752+ethanMI32@users.noreply.github.com>
2023-08-05 22:57:33 -0700

    Add files via upload

--- login.js
+++ login.js
@@ -1,15 +1,15 @@
- function validateCredentials(event) {
+function validateCredentials(event) {
     event.preventDefault();
     
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     
     // You can replace these values with the correct username and password.
-    const validUsername = "JamesBond";
-    const validPassword = "James5678";
+    const validUsername = "your_username";
+    const validPassword = "your_password";
     
     if (username === validUsername && password === validPassword) {
-        alert("Login successful! Please click link");
+        alert("Login successful! Please click button.");
         // Redirect to the new page if the credentials are correct.
         window.location.href = "new_page.html";
     } else {
