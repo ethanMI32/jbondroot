28ba96fc940d53b13fee9b984e00d69ca0a103ce
ethanMI32 <140582752+ethanMI32@users.noreply.github.com>
2023-08-07 21:58:48 -0700

    Add files via upload

--- login.js
+++ login.js
@@ -3,14 +3,14 @@ function validateCredentials(event) {
     event.preventDefault();
 
 var users = [
-    { id: 1, username: "John", password: "Smith", Money: "$0" },
-    { id: 2, username: "vesper", password: "lynd", Money: "$5" },
-    { id: 3, username: "john", password: "parola3", Money: "$10" },
-    { id: 4, username: "james007", password: "parola4", Money: "$15,0000.000" },
-    { id: 5, username: "buckye", password: "parola5", Money: "$12" },
-    { id: 6, username: "michael", password: "parola6", Money: "$100" },
-    { id: 7, username: "steve", password: "parola7", Money: "$0" },
-    { id: 8, username: "smith", password: "parola8", Money: "$10" }
+    { id: 1, username: "James", password: "Bond"},
+    { id: 2, username: "Vesper", password: "Lynd"},
+    { id: 3, username: "Valdo", password: "Obruchev"},
+    { id: 4, username: "james007", password: "parola4"},
+    { id: 5, username: "buckye", password: "parola5"},
+    { id: 6, username: "michael", password: "parola6"},
+    { id: 7, username: "steve", password: "parola7"},
+    { id: 8, username: "smith", password: "parola8"}
 ];
   
 
