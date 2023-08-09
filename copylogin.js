4a253bdb37aae28fa442dbf99337c8fac8c98720
ethanMI32 <140582752+ethanMI32@users.noreply.github.com>
2023-08-08 22:51:08 -0700

    Add files via upload

---
+++ copylogin.js
@@ -0,0 +1,43 @@
+function validateCredentials(event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    // List of valid usernames and passwords
+
+
+    const validCredentials = [
+        { username: "ValdoObruchev", password: "Primo_2021" },
+    ];
+    // Check if the entered credentials are valid
+    const isValidCredential = validCredentials.some(cred => cred.username === username && cred.password === password);
+    if (isValidCredential) {
+        alert("Login successful! Please click the button.");
+        // Redirect to the new page if the credentials are correct.
+        window.location.href = "https://rootflag.vercel.app/admin_mainpage.html";
+
+        
+    const validCredentials = [
+        { username: "FranzOberhauser", password: "h@t3b0nd" },
+    ];
+    // Check if the entered credentials are valid
+    const isValidCredential = validCredentials.some(cred => cred.username === username && cred.password === password);
+    if (isValidCredential) {
+        alert("Login successful! Please click the button.");
+        // Redirect to the new page if the credentials are correct.
+        window.location.href = "https://rootflag.vercel.app/MrOberhauser.html";
+
+
+    const validCredentials = [
+        { username: "Le Chiffre", password: "Pl@yap0k3r" }
+    ];
+    // Check if the entered credentials are valid
+    const isValidCredential = validCredentials.some(cred => cred.username === username && cred.password === password);
+    if (isValidCredential) {
+    alert("Login successful! Please click the button.");
+    // Redirect to the new page if the credentials are correct.
+    window.location.href = "https://rootflag.vercel.app/lechiffre.html";
+
+    } else {
+        alert("Invalid credentials. Please try again.");
+    }
+}
