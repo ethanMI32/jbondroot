020eb4c347ba84ed2d3849825191f3fcb1c1a457
ethanMI32 <140582752+ethanMI32@users.noreply.github.com>
2023-08-08 21:55:25 -0700

    Add files via upload

--- login.js
+++ login.js
@@ -1,23 +1,19 @@
-// JavaScript source code
 function validateCredentials(event) {
     event.preventDefault();
-
-var users = [
-    { id: 1, username: "James", password: "Bond"},
-    { id: 2, username: "Vesper", password: "Lynd"},
-    { id: 3, username: "Valdo", password: "Obruchev"},
-    { id: 4, username: "james007", password: "parola4"},
-    { id: 5, username: "buckye", password: "parola5"},
-    { id: 6, username: "michael", password: "parola6"},
-    { id: 7, username: "steve", password: "parola7"},
-    { id: 8, username: "smith", password: "parola8"}
-];
-  
-
-    if (username === Username && password === Password) {
-        alert("Login successful! Please click button.");
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    // List of valid usernames and passwords
+    const validCredentials = [
+        { username: "ValdoObruchev", password: "Primo_2021" },
+        { username: "james007", password: "y0ug0t1t" },
+        { username: "mrwhite", password: "n0g@m3" }
+    ];
+    // Check if the entered credentials are valid
+    const isValidCredential = validCredentials.some(cred => cred.username === username && cred.password === password);
+    if (isValidCredential) {
+        alert("Login successful! Please click the button.");
         // Redirect to the new page if the credentials are correct.
-        window.location.href = "admin_page.html";
+        window.location.href = "https://rootflag.vercel.app/admin_mainpage.js";
     } else {
         alert("Invalid credentials. Please try again.");
     }
