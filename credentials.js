4c7e47e0e2e4c61d1c67ae97d605a41e9c56ecee
ligiaHi <140875904+ligiaHi@users.noreply.github.com>
2023-08-12 18:23:59 -0700

    credentials.js

--- credentials.js
+++ credentials.js
@@ -1,28 +1,43 @@
-function validateCredentials(event) {
-    event.preventDefault();
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-    
-    // List of valid usernames and passwords
-    const validCredentials = [
-        { username: "LeChiffre", password: "Pl@yap0k3r", page: "lechiffre.html" },
-        { username: "LadyM", password: "S@ft3ypriority", page: "" },
-        { username: "VesperLynd", password: "LuvB0nd", page: "" },
-        { username: "FelixLeiter", password: "b0rn1nus@", page: "" },
-        { username: "MrWhite", password: "Tr@1t0r", page: "" },
-        { username: "RaoulSilva", password: "H@t3bond", page: "" },
-        { username: "ErnstBlofeld", password: "3rns1bl0f3ld", page: "ernstblofeld.html" },
-        { username: "ValdoObruchev", password: "Primo_2021", page: "admin_mainpage.html" },
-    ];
-    
-    // Find the user's entry in the valid credentials list
-    const userEntry = validCredentials.find(cred => cred.username === username && cred.password === password);
-    
-    if (userEntry) {
-        alert("Login successful! Redirecting...");
-        // Redirect to the user's specific page
-        window.location.href = userEntry.page;
-    } else {
-        alert("Invalid credentials. Please try again.");
-    }
-}
+// login.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    const loginForm = document.getElementById("loginForm");
+
+    const validCredentials = {
+        "LeChiffre": "Pl@yap0k3r",
+        "LadyM": "S@ft3ypriority",
+        "VesperLynd": "LuvB0nd",
+        "FelixLeiter": "b0rn1nus@",
+        "MrWhite": "Tr@1t0r",
+        "RaoulSilva": "H@t3bond",
+        "ErnstBlofeld": "3rns1bl0f3ld",
+        "ValdoObruchev": "Primo_2021"
+    };
+
+    loginForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        // Check if the entered username exists and the password matches
+        if (validCredentials.hasOwnProperty(username) && validCredentials[username] === password) {
+            alert("Login successful! Redirecting...");
+            
+            // Redirect to the user's specific page (using a simple mapping for demonstration)
+            const userPages = {
+                "LeChiffre": "lechiffre.html",
+                "ErnstBlofeld": "ernstblofeld.html",
+                "ValdoObruchev": "admin_mainpage.html"
+            };
+            
+            if (userPages.hasOwnProperty(username)) {
+                window.location.href = userPages[username];
+            } else {
+                alert("Welcome, " + username);
+            }
+        } else {
+            alert("Invalid credentials. Please try again.");
+        }
+    });
+});
